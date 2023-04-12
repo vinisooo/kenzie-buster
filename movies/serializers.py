32a54750a9384933752a5e1fbe397a1e2c05ef24
vinisooo <cviniciusliramdev@gmail.com>
2023-04-12 18:03:11 -0300

    fix: adjust movie serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -5,5 +5,7 @@ from .models import RatingChoices
 class MovieSerializer:
     title = serializers.CharField(max_length=127)
     duration = serializers.CharField(max_length=10, required=False)
-    ratings = serializers.ChoiceField(max_length=20, choices=RatingChoices.choices)
+    ratings = serializers.ChoiceField(
+        max_length=20, choices=RatingChoices.choices, required=False
+    )
     synopsis = serializers.TextField(required=False)
