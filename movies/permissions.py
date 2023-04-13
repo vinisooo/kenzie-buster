c9a928713f633a7b22be5f52f4ec0bbd2c64fb88
vinisooo <cviniciusliramdev@gmail.com>
2023-04-12 22:57:49 -0300

    feat: add get movies route feature

---
+++ movies/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+from rest_framework.views import Request, View
+
+
+class MoviesPermission(permissions.BasePermission):
+    def has_permission(self, request: Request, view: View):
+        if request.method == "POST":
+            return request.user.is_employee
+
+        return request.method in permissions.SAFE_METHODS
--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import MovieSerializer
+from .models import Movie
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import MoviesPermission
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class MovieView(APIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticatedOrReadOnly, MoviesPermission]
 
     def post(self, request):
         serializer = MovieSerializer(data=request.data)
@@ -17,3 +19,9 @@ class MovieView(APIView):
         serializer.save()
 
         return Response(serializer.data, 201)
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+
+        return Response(serializer.data, 200)
